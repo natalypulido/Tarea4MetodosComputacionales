128b1c7e471b6a9831e2b2cd3f6618dc877b0434
Nataly Pulido <n.pulido200@uniandes.edu.co>
2017-07-27 21:48:08 -0500

    Terminado - Corre

--- Plots.py
+++ Plots.py
@@ -3,3 +3,15 @@ import matplotlib.pyplot as plt
 
 data= np.loadtxt("map_data.txt")
 res = np.loadtxt("resultados.txt")
+
+fig = plt.gcf()
+ax = fig.gca()
+c = plt.Circle((res[1],res[0]), res[2], fill = False)
+plt.scatter([res[1]],[res[0]], color = 'g')
+ax.add_artist(c)
+imgplot = plt.imshow(data)
+plt.xlabel("Latitud")
+plt.ylabel("Longitud")
+fig.suptitle("Mapa Continentes", fontsize=20)
+plt.savefig("PuntoNemo.pdf")
+plt.close()
