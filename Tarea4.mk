8b643b464b896b99fa8dfbdca23683b5732cc17b
Nataly Pulido <n.pulido200@uniandes.edu.co>
2017-07-27 21:06:18 -0500

    TERMINADO con correccion de restricciones

--- Tarea4.mk
+++ Tarea4.mk
@@ -1,12 +1,12 @@
 #Tarea4.mk
 
-PuntoNemo.pdf: Plots.py
+PuntoNemo.pdf: Plots.py resultados.txt
 	python Plots.py
-Plots.py: resultados.txt
-	python resultados.txt
 resultados.txt: PuntoGeographicPoint.x
 	./PuntoGeographicPoint.x
 PuntoGeographicPoint.x: PuntoGeographicPoint.c
 	cc PuntoGeographicPoint.c -lm -o PuntoGeographicPoint.x
 PuntoGeographicPoint.c: map_data.txt
 	cc PuntoGeographicPoint.c -lm -o PuntoGeographicPoint.x
+
+
