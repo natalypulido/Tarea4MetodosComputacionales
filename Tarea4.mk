e952cb3dafe9f233fee471116fefae4d7d17945d
Nataly Pulido <n.pulido200@uniandes.edu.co>
2017-07-27 20:05:10 -0500

    Correcion de nombre PuntoGeogra(..) y de mayuscula

--- Tarea4.mk
+++ Tarea4.mk
@@ -2,9 +2,9 @@
 
 PuntoNemo.pdf: Plots.py
 	python Plots.py
-Resultados.txt: PuntoGeographic.x
-	./PuntoGeographic.x
-PuntoGeographic.x: PuntoGeographic.c
-	cc PuntoGeographic.c -lm -o PuntoGeographic.x
-PuntoGeographic.c: map_data.txt
-	cc PuntoGeographic.c -lm -o PuntoGeographic.x
+resultados.txt: PuntoGeographicPoint.x
+	./PuntoGeographicPoint.x
+PuntoGeographicPoint.x: PuntoGeographicPoint.c
+	cc PuntoGeographicPoint.c -lm -o PuntoGeographicPoint.x
+PuntoGeographicPoint.c: map_data.txt
+	cc PuntoGeographicPoint.c -lm -o PuntoGeographicPoint.x
